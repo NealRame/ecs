fcd90aacbd30cf91bebf79896abf47d8dc787a43
Julien Graziano <julien@graziano.fr>
2022-10-31 00:12:08 +0100

    tests: test #getEntityComponents

--- src/tests/ecs.ts
+++ src/tests/ecs.ts
@@ -114,6 +114,77 @@ describe("ECS", () => {
                 .to.have.property("getEntityComponents")
                 .that.is.a("function")
         })
+        it("should return an IComponentContainer object", () => {
+            const ecs = new ECS()
+            const entity = ecs.addEntity()
+            const components = ecs.getEntityComponents(entity)
+            expect(components).to.be.an("object")
+
+            expect(components)
+                .to.have.property("has")
+                .that.is.a("function")
+            expect(components)
+                .to.have.property("hasAll")
+                .that.is.a("function")
+        })
+        describe("IComponentContainer", () => {
+            const ecs = new ECS()
+            const entity = ecs.addEntity()
+
+            const SomeComponent1 = class extends Component {}
+            const SomeComponent2 = class extends Component {}
+            const SomeComponent3 = class extends Component {}
+
+            ecs.addEntityComponent(entity, new SomeComponent1)
+            ecs.addEntityComponent(entity, new SomeComponent2)
+
+            const components = ecs.getEntityComponents(entity)
+
+            describe("#get", () => {
+                it("should be a method", () => {
+                    expect(components)
+                        .to.have.property("get")
+                        .that.is.a("function")
+                })
+                it("should return the component if it exists", () => {
+                    expect(components.get(SomeComponent1))
+                        .to.be.an.instanceof(SomeComponent1)
+                })
+                it("should throw an error if the component does not exist", () => {
+                    expect(() => components.get(SomeComponent3)).to.throw()
+                })
+            })
+            describe("#has", () => {
+                it("should be a method", () => {
+                    expect(components)
+                        .to.have.property("has")
+                        .that.is.a("function")
+                })
+                it("should return true if entity has the given component", () => {
+                    expect(components.has(SomeComponent1))
+                        .to.be.true
+                })
+                it("should return false if entity does not have the given component", () => {
+                    expect(components.has(SomeComponent3))
+                        .to.be.false
+                })
+            })
+            describe("#hasAll", () => {
+                it("should be a method", () => {
+                    expect(components)
+                        .to.have.property("hasAll")
+                        .that.is.a("function")
+                })
+                it("should return true if entity has all components", () => {
+                    expect(components.hasAll([SomeComponent1, SomeComponent2]))
+                        .to.be.true
+                })
+                it("should return false if entity does not have all components", () => {
+                    expect(components.hasAll([SomeComponent1, SomeComponent3]))
+                        .to.be.false
+                })
+            })
+        })
     })
     describe("#getEntityComponent()", () => {
         it("should be a method", () => {
