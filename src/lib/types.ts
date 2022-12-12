c1b51acada55504ca5d0d7ec317aa2c0de197f71
Julien Graziano <julien@graziano.fr>
2022-12-13 00:02:14 +0100

    style: indent

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -87,7 +87,7 @@ export interface IRegistry {
 
     getSystem(System: TConstructor<ISystem>): ISystem
     hasSystem(System: TConstructor<ISystem>): boolean
-    
+
     reset(): void
     update(): void
 
